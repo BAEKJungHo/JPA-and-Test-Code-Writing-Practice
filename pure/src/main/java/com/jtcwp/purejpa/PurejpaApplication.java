6e633b64b39fb64a7a24fdd3ad54b3b2143b54e5
BAEKJungHo <designjava@naver.com>
2021-12-06 19:35:43 +0900

    Create Pure Projects
    - Create JPA persistence.xml

---
+++ pure/src/main/java/com/jtcwp/purejpa/PurejpaApplication.java
@@ -0,0 +1,13 @@
+package com.jtcwp.purejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurejpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurejpaApplication.class, args);
+    }
+
+}
---
+++ pure/src/test/java/com/jtcwp/purejpa/PurejpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtcwp.purejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurejpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
