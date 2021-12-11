70784297f22594733121993b43663ffd4223dfb3
BAEKJungHo <designjava@naver.com>
2021-12-11 17:51:50 +0900

    Member 등록, 조회 테스트

--- pure/src/test/java/com/jtcwp/purejpa/domain/member/MemberTest.java
+++ pure/src/test/java/com/jtcwp/purejpa/domain/member/MemberTest.java
@@ -1,10 +1,12 @@
 package com.jtcwp.purejpa.domain.member;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -21,25 +23,31 @@ class MemberTest {
     @Value("${persistence.unitname}")
     private String persistenceUnitName;
 
-    @DisplayName("Member 등록 테스트")
+    @DisplayName("Member 등록 후 조회 테스트")
     @Test
-    void createMemberTable() throws Exception {
+    void insertMemberAfterFindMemberTest() {
         // given
         EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnitName);
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
-        tx.begin();
 
         // when
+        tx.begin();
+
+        // insert
         Member member = new Member();
-        member.setId(1L);
         member.setUserName("Jungho");
         em.persist(member);
-        em.close();
-        emf.close();
         tx.commit();
+        em.close();
+
+        // find
+        EntityManager em2 = emf.createEntityManager();
+        Member findMember = em2.find(Member.class, 1L);
+        emf.close();
 
         // then
+        assertThat(findMember.getUserName()).isEqualTo("Jungho");
     }
 
 }
